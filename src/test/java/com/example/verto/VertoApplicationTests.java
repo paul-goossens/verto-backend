e14e173fea00e8a277a4beb0a9b4845bf2e85003
Paul Goossens <pgoossens@move4mobile.com>
2021-12-28 13:04:06 +0100

    Big Bang :boom:

---
+++ src/test/java/com/example/verto/VertoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.verto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VertoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
