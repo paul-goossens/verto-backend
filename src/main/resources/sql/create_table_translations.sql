d6a406f1ce4b5f429ad557f2a400246ede96385b
Paul Goossens <pgoossens@move4mobile.com>
2022-01-07 10:51:40 +0100

    increased char length for translation key to 128

--- src/main/resources/sql/create_table_translations.sql
+++ src/main/resources/sql/create_table_translations.sql
@@ -1,7 +1,7 @@
 create table translations
 (
 	guid varchar(64) not null,
-	key varchar(32) not null,
+	key varchar(128) not null,
 	value varchar(1024),
 	is_group bool not null,
 	language_guid varchar(64)
